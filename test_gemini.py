c612c69b88059b466fdf7198ba42f919cba69233
Akshat Garg <ak15garg2005@gmail.com>
2026-04-23 16:40:57 +0530

    Remove test scripts

--- test_gemini.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-API_KEY = 'AIzaSyDSHJBRqncfuGmstbbzB8XznaS9UY8Z1RA'
-URL = f'https://generativelanguage.googleapis.com/v1beta/models?key={API_KEY}'
-
-response = requests.get(URL)
-print("Status Code:", response.status_code)
-for model in response.json().get('models', []):
-    print(model['name'])
